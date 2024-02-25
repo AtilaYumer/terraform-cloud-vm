a4774dbf0d3944d0e87b991173bce1ffcb942385
Atila Yumer <atillayumer@gmail.com>
2024-02-25 23:34:39 +0200

    Update main.tf

--- modules/gcp/main.tf
+++ modules/gcp/main.tf
@@ -37,7 +37,7 @@ resource "google_compute_instance" "compute_instance" {
 
   boot_disk {
     initialize_params {
-      image = "docker.io/ubuntu:18.04"
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
     }
   }
 
